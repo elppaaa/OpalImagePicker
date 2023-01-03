ae3d3444a76470819e3f33dc138b3a33d427ec69
elppaaa <withbcd@gmail.com>
2023-01-03 17:29:26 +0900

    SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "OpalImagePicker"
+    name: "OpalImagePicker",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "OpalImagePicker",
+            targets: ["OpalImagePicker"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "OpalImagePicker",
+            resources: [
+              .process("Resources/")
+            ]
+        ),
+    ]
 )
